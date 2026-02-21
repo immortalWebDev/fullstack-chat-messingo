64c3ff2c0198fe17cb4d7da22bd42ff45973bedb
immortalWebDev <pgbadgujar007@gmail.com>
2026-02-22 00:22:18 +0530

    fix: update CORS configuration and improve private message handling

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,10 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//   },
-// });
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5175", "http://localhost:5174"],
+    origin: ["http://localhost:5175", "http://localhost:5174","https://fullstack-chat-messingo.vercel.app"],
     credentials: true,
   },
 });
@@ -34,6 +29,7 @@ io.on("connection", (socket) => {
 
 
 socket.on("private_message", ({ to, sender, text }) => {
+    if (!to || !sender || !text) return;
   const targetSocket = users[to];
 
   const messageData = {
@@ -55,11 +51,13 @@ socket.on("private_message", ({ to, sender, text }) => {
     for (let username in users) {
       if (users[username] === socket.id) {
         delete users[username];
+        break;
       }
     }
   });
 });
+const PORT = process.env.PORT || 5000;
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
